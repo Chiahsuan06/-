f58c4265c415e4bef88e528ae392b62c2bc3d0af
Chiahsuan06 <chiahsuan56566498@gmail.com>
2021-11-03 23:15:57 +0800

    [完善前台確認頁-1] 送出鈕詢問是否送出，未做 問卷問題區未做

--- 動態問卷系統/前台/前台確認頁.aspx
+++ 動態問卷系統/前台/前台確認頁.aspx
@@ -9,7 +9,32 @@
 </head>
 <body>
     <form id="form1" runat="server">
+         <div>   <%--標題--%>
+            <asp:Repeater ID="reHeading" runat="server">
+                <ItemTemplate>
+                    <h2>
+                        <%#Eval("Heading") %>
+                    </h2>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
         <div>
+            <asp:Label ID="lblName" runat="server" Text="姓名："></asp:Label>
+            <asp:Literal ID="ltlName" runat="server"></asp:Literal><br /><br />
+            <asp:Label ID="lblPhone" runat="server" Text="手機："></asp:Label>
+            <asp:Literal ID="ltlPhone" runat="server"></asp:Literal><br /><br />
+            <asp:Label ID="lblEmail" runat="server" Text="Email："></asp:Label>
+            <asp:Literal ID="ltlEmail" runat="server"></asp:Literal><br /><br />
+            <asp:Label ID="lblAge" runat="server" Text="年齡："></asp:Label>
+            <asp:Literal ID="ltlAge" runat="server"></asp:Literal>
+        </div>
+         <div>  <%--題目--%>
+
+        </div>
+        <div>
+            <asp:Button ID="btnCancel" runat="server" Text="修改" OnClick="btnCancel_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnSent" runat="server" Text="送出" OnClick="btnSent_Click" />
         </div>
     </form>
 </body>
--- 動態問卷系統/前台/前台確認頁.aspx.cs
+++ 動態問卷系統/前台/前台確認頁.aspx.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using DBSource;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 namespace 動態問卷系統.前台
 {
@@ -11,7 +15,100 @@ namespace 動態問卷系統.前台
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string IDNumber = "1";   //先寫死 "1"
+                                     //this.Session["QuestionnaireNum"] as string
+            this.reHeading.DataSource = GetHeading(IDNumber);
+            this.reHeading.DataBind();
 
+            this.ltlName.Text = Session["Name"] as string; 
+            this.ltlPhone.Text = Session["Phone"] as string; 
+            this.ltlEmail.Text = Session["Email"] as string; 
+            this.ltlAge.Text = Session["Age"] as string;
+        }
+
+        /// <summary>
+        /// 顯示資料
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GetHeading(string IDNumber)
+        {
+            string connStr = DBHelper.GetConnectionString();
+            string dbcommand =
+                $@"SELECT [Heading]
+                    FROM [Outline]
+                    WHERE Number = @IDNumber
+                ";
+
+            List<SqlParameter> list = new List<SqlParameter>();
+            list.Add(new SqlParameter("@IDNumber", IDNumber));
+
+            try
+            {
+                return DBHelper.ReadDataTable(connStr, dbcommand, list);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 會回到填寫頁
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/ 前台 / 前台內頁.aspx");
+        }
+
+        /// <summary>
+ /// 送出
+ /// </summary>
+ /// <param name="sender"></param>
+ /// <param name="e"></param>
+        protected void btnSent_Click(object sender, EventArgs e)
+        {
+            string Name = this.ltlName.Text;
+            int Phone = Convert.ToInt32(this.ltlPhone.Text);
+            string Email = this.ltlEmail.Text;
+            int Age = Convert.ToInt32(this.ltlAge.Text);
+
+            //這邊要做一個詢問是否送出
+            sentData(Name, Phone, Email, Age);
+            MessageBox.Show($"資料送出成功，即將返回列表頁", "確定", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Response.Redirect("/ 前台 / 前台列表頁.aspx");
+        }
+
+        /// <summary>
+        /// 寫進資料庫
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static DataTable sentData(string Name, int Phone, string Email, int Age)
+        {
+            string connStr = DBHelper.GetConnectionString();
+            string dbcommand =
+                $@" INSERT INTO [Information] ([UserID], [Name], [Phone], [Email], [Age])
+                    SELECT newid() AS [UserID], @Name, @Phone, @Email, @Age
+                ";
+
+            List<SqlParameter> list = new List<SqlParameter>();
+            list.Add(new SqlParameter("@Name", Name));
+            list.Add(new SqlParameter("@Phone", Phone));
+            list.Add(new SqlParameter("@Email", Email));
+            list.Add(new SqlParameter("@Age", Age));
+
+            try
+            {
+                return DBHelper.ReadDataTable(connStr, dbcommand, list);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
         }
     }
 }
